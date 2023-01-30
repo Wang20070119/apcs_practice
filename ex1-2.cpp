04797f5bd9073661956a0796fa980699e61445ab
Wang20070119 <123912186+Wang20070119@users.noreply.github.com>
2023-01-30 11:19:20 +0800

    Create ex1-2.cpp

---
+++ ex1-2.cpp
@@ -0,0 +1,11 @@
+    printf("Hello World Wang,your tall is %d m %d cm ",1,75);
+    printf("Hello World Wang,your tall is %d m %d cm \n",1,75);
+   
+    printf("my height is %d m %d cm \n",1,80);
+    printf("this building is %5d cm \n",890);
+    printf("this building is %5d cm \n",7890);
+    printf("this building is %5d cm \n",67890);
+    printf("this building is %5d cm \n",567890);
+    printf("this building is %5d cm \n",1234567890);
+    printf("pi = %f \n",3.1415926535897932384626433832795);
+    return 0; 
